d64aafcc8bc897f25f02c2bc7a0f3b5c4aad7e64
Rakib <112962930+Rakib010@users.noreply.github.com>
2023-09-07 22:23:25 +0600

    Add files via upload

---
+++ 467A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin>>n;
+    int p,q,cnt=0,cont=0;
+    while(n--){
+      cin>>p>>q;
+      if(q-p>=2){
+        cnt++;
+      }
+
+    }
+    cout<<cnt;
+
+
+}
