4d6bf20e7e260030e9f67244876eddf6cbe8a3bf
Rakib <112962930+Rakib010@users.noreply.github.com>
2023-09-07 22:21:26 +0600

    Add files via upload

---
+++ 281.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   string s;
+   cin>>s; int i;
+       if(s[i]==toupper(s[i])){
+        cout<<s;
+       } else {
+       (s[i]=tolower(s[i]));
+        s[0]=toupper(s[0]);{
+                cout<<s;
+        }
+           
+     }       
+   
+}
