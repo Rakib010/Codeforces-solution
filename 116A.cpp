55f52056d983e4ff1db68cc26eda74095fc6a610
Rakib <112962930+Rakib010@users.noreply.github.com>
2023-09-07 22:19:40 +0600

    Add files via upload

---
+++ 116A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,n; cin>>t>>n;
+    int in,out;
+    int sum=0,x=0;
+    for(int i=0;i<n;i++){
+         cin>>in>>out;
+         
+        }
+        cout<<sum;
+    }
+   /* sum=(in+x)-out;
+         if(sum<0)
+          x=0;
+        else
+          x=sum;
+     */     
